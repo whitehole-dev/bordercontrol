20fc0a448f5bacbcd71174af40a7f1cc744e4f8d
MoMMde <mommde@protonmail.com>
2023-01-22 01:28:54 +0100

    remv. common modules

--- bordercontrol-common/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    java
-}
-
-description = "Common Java classes published to Repository for 3rd-party development"
--- bordercontrol-common/src/main/java/net/whitehole/bordercontrol/common/util/Headers.java
+++
@@ -1,6 +0,0 @@
-package net.whitehole.bordercontrol.common.util;
-
-public class Headers {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String PERMISSIONS = "Permissions";
-}
